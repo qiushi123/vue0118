5a353787543c8fdaf136b34fcfba8993bd52c5b8
邱石 <5617953+zhihuixiaozhen@user.noreply.gitee.com>
2020-01-17 19:50:11 +0800

    登陆页页面使用elementui

---
+++ src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <el-form :rules="rules" :model="loginForm" class="loginRoot">
+            <h3 class="loginTitle">系统登陆</h3>
+            <el-form-item prop="username">
+                <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"/>
+            </el-form-item>
+            <el-form-item prop="password">
+                <el-input type="text" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"/>
+            </el-form-item>
+            <el-checkbox v-model="checked" class="loginRem"></el-checkbox>
+            <el-button type="primary" style="width: 100%">登陆</el-button>
+        </el-form>
+    </div>
+</template>
+<script>
+    export default {
+        name: "Login",
+        data() {
+            return {
+                loginForm: {
+                    username: 'admin',
+                    password: '123'
+                },
+                checked: true,
+                rules: {
+                    username: [{required: true, message: '请输入用户名', trigger: 'blur'}],
+                    password:
+                        [{required: true, message: '请输入密码', trigger: 'blur'}],
+                },
+            }
+        }
+    }
+</script>
+
+<style>
+    .loginRoot {
+        border-radius: 15px;
+        background-clip: padding-box;
+        margin: 50px auto;
+        width: 350px;
+        padding: 15px 35px;
+        background: aliceblue;
+        border: 1px solid #eaeaea;
+        box-shadow: 0 0 25px #c6c6c6;
+    }
+
+    .loginTitle {
+        margin: 15px auto 20px auto;
+        text-align: center;
+        color: #505050;
+    }
+
+    .loginRem {
+        text-align: left;
+        margin: 0px 0px 15px 0px;
+    }
+</style>
